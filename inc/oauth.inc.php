940d9a5820f755a72fcc7fcc5604bd543735ba5a
Cédric Salvador <csalvador@gpcsolutions.fr>
2013-06-03 10:26:52 +0200

    changed scope

--- inc/oauth.inc.php
+++ inc/oauth.inc.php
@@ -55,7 +55,8 @@ define(
     'GOOGLE_CONTACTS_BATCH_URI',
     GOOGLE_CONTACTS_URI . '/default/' . GOOGLE_PROJECTION . '/batch'
 );
-define('GOOGLE_AUTH_SCOPE', 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile');
+define('GOOGLE_USERINFO_EMAIL_SCOPE', 'https://www.googleapis.com/auth/userinfo.email');
+define('GOOGLE_USERINFO_PROFILE_SCOPE', 'https://www.googleapis.com/auth/userinfo.profile');
 /**
  * Token info URI
  */
