d4dc2a54bcce93d8acfea9b89807af53eed01532
Raphaël Doursenaud <rdoursenaud@gpcsolutions.fr>
2014-05-26 17:27:04 +0200

    Explicitely declared auth exception

--- class/Zenfusion_Oauth2Client.class.php
+++ class/Zenfusion_Oauth2Client.class.php
@@ -25,6 +25,7 @@
  */
 set_include_path(dol_buildpath('/zenfusionoauth/lib/google-api-php-client/src'));
 dol_include_once('/zenfusionoauth/lib/google-api-php-client/src/Google/Client.php');
+dol_include_once('/zenfusionoauth/lib/google-api-php-client/src/Google/Auth/Exception.php');
 dol_include_once('/zenfusionoauth/inc/oauth.inc.php');
 
 /**
